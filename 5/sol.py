3ad10f28fb853d3ec51f6c46ba1601dc3727f1da
Tim Reynolds <tim@cubingusa.org>
2022-12-05 07:09:33 -0500

    day 5

---
+++ 5/sol.py
@@ -0,0 +1,22 @@
+import re
+
+state = [[] for i in range(9)]
+
+for line in open('input.txt'):
+  line = line.rstrip()
+  if '[' in line:
+    for i in range(9):
+      if len(line) >= i * 4 + 2:
+        nextval = line[i * 4 + 1]
+        if nextval != ' ':
+          state[i].insert(0, nextval)
+  elif 'move' in line:
+    m = re.match('move ([0-9]*) from ([0-9]*) to ([0-9]*)', line)
+    to_move = int(m.group(1))
+    fr = int(m.group(2))
+    to = int(m.group(3))
+    for i in range(to_move):
+      state[to - 1] += [state[fr - 1].pop()]
+
+print(state)
+print(''.join([s[-1] for s in state]))
